70154a25efed4827c919e801f06c781a1832eddf
Alexander Pagliarini <Alexander2.Pagliarini@live.uwe.ac.uk>
2017-09-30 20:53:23 +0100

    Actually made a pretty decent start

--- Water/Assets/Waves.cs
+++ Water/Assets/Waves.cs
@@ -1,16 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class Waves : MonoBehaviour {
+public class Waves : MonoBehaviour
+{
+    public float multiplier;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Update()
+    {
+        formWaves();
+    }
+
+    void formWaves()
+    {
+        MeshFilter mF = GetComponent<MeshFilter>();
+        MeshCollider mC = GetComponent<MeshCollider>();
+
+        mC.sharedMesh = mF.mesh;
+
+        Vector3[] verts = mF.mesh.vertices;
+
+        for (int i = 0; i < verts.Length; i++)
+        {
+            verts[i].y = Mathf.Sin(Time.deltaTime * multiplier);
+        }
+
+        mF.mesh.vertices = verts;
+        mF.mesh.RecalculateNormals();
+        mF.mesh.RecalculateBounds();
+    }
+
 }
